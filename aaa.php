b24123120ac3be2ba55c65b2a38737bdf5e117fc
zhangzhanjin <zhangzhanjin@pxjy.com>
2020-12-29 14:05:41 +0800

    df

--- aaa.php
+++ aaa.php
@@ -1,4 +1,16 @@
 <?php
-
-
+$buy = [34.3,35.41,35.81,35.90,35.96,35.89];
+$sell = [34.49,35.68,36.07,35.98,35.99];
+$sumBuy=$sumSell=0;
+foreach ($buy as $k => $v) {
+  $sumBuy+=$v;
+}
+$resB = $sumBuy/count($buy);
+echo $resB;
+echo "<br />";
+foreach ($sell as $k => $v) {
+  $sumSell+=$v;
+}
+$resS = $sumSell/count($sell);
+echo $resS;
 ?>
