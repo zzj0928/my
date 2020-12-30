645e492a081934db8280d5bb7ea4731f5b3b27b4
zhangzhanjin <zhangzhanjin@pxjy.com>
2020-12-30 10:06:33 +0800

    dfd

--- index.php
+++ index.php
@@ -6,6 +6,26 @@ foreach ($buy as $k => $v) {
   $sumBuy+=$v;
 }
 $resB = $sumBuy/count($buy);
+echo '东亚：';
+echo "<br />";
+echo $resB;
+echo "<br />";
+foreach ($sell as $k => $v) {
+  $sumSell+=$v;
+}
+$resS = $sumSell/count($sell);
+echo $resS;
+
+echo "<br />";
+$buy = [13.02,12.85,12.97,12.77,12.50,12.63,12.46,12.54,12.49,12.44,12.26,11.85,11.65,11.88,11.83,11.78,10.40];
+$sell = [12.50,12.93,12.96,12.72,12.57,12.57,12.47,11.88,11.40,11.84,11.88];
+$sumBuy=$sumSell=0;
+foreach ($buy as $k => $v) {
+  $sumBuy+=$v;
+}
+$resB = $sumBuy/count($buy);
+echo '通达：';
+echo "<br />";
 echo $resB;
 echo "<br />";
 foreach ($sell as $k => $v) {
