cbbc03e3fbd76e4bb71a6e065b5f384213b131d4
zhangzhanjin <zhangzhanjin@pxjy.com>
2021-01-29 16:59:24 +0800

    d

--- index.php
+++ index.php
@@ -1,36 +1,29 @@
 <?php
-$buy = [34.3,35.41,35.81,35.90,35.96,35.89,34.65,34.55,34.67];
-$sell = [34.49,35.68,36.07,35.98,35.99,34.88,34.76];
-$sumBuy=$sumSell=0;
-foreach ($buy as $k => $v) {
-  $sumBuy+=$v;
+$a = 35;
+$c = 100;
+$p = $a*$c;
+echo $a;
+echo "<br/>";
+echo $c;
+echo "<br/>";
+$p = $a * $c;
+echo $p;
+echo "<br/>";
+//1+1+2+4+8
+for ($i=0; $i < 5; $i++) { 
+	$c +=$c;
+	$a -=1;
+	$all = $p;
+	echo $a;
+	echo "<br/>";
+	echo $c;
+	echo "<br/>";
+	$p = $a * $c;
+	echo $p;
+	echo "<br/>";
+	$all += $p;
 }
-$resB = $sumBuy/count($buy);
-echo '东亚：';
-echo "<br />";
-echo $resB;
-echo "<br />";
-foreach ($sell as $k => $v) {
-  $sumSell+=$v;
-}
-$resS = $sumSell/count($sell);
-echo $resS;
 
-echo "<br />";
-$buy = [13.02,12.85,12.97,12.77,12.50,12.63,12.46,12.54,12.49,12.44,12.26,11.85,11.65,11.88,11.83,11.78,10.40];
-$sell = [12.50,12.93,12.96,12.72,12.57,12.57,12.47,11.88,11.40,11.84,11.88,10.47];
-$sumBuy=$sumSell=0;
-foreach ($buy as $k => $v) {
-  $sumBuy+=$v;
-}
-$resB = $sumBuy/count($buy);
-echo '通达：';
-echo "<br />";
-echo $resB;
-echo "<br />";
-foreach ($sell as $k => $v) {
-  $sumSell+=$v;
-}
-$resS = $sumSell/count($sell);
-echo $resS;
+echo $all;
+echo "<br/>";
 ?>
